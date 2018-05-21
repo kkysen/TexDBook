6b75f5439bca02dfcb8781329dbc77c5f5609fa8
Khyber <kkysen@gmail.com>
2018-05-20 21:32:50 -0400

    Trying to get Flask to work with Apache2 correctly.

--- TexDBook/src/python/util/log.py
+++ TexDBook/src/python/util/log.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import logging
 import os
 from logging import Logger
@@ -6,8 +8,13 @@ from TexDBook.src.python.core.init import NAME
 
 log_path = "/var/log/apache2/TexDBook.log"
 
-os.open(log_path, os.O_RDWR | os.O_CREAT, 0o666)
+if False:
+    os.open(log_path, os.O_RDWR | os.O_CREAT, 0o666)
+    
+    logging.basicConfig(filename=log_path)
+    
+    log = logging.getLogger(NAME)  # type: Logger
 
-logging.basicConfig(filename=log_path)
+log = object()
 
-log = logging.getLogger(NAME)  # type: Logger
+setattr(log, "debug", print)
