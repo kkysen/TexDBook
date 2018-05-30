137e1563b9ebd2ed102bfbf4e4c5ff12410725ce
Khyber <kkysen@gmail.com>
2018-05-30 12:24:26 -0400

    Trying to get server to work.

--- TexDBook/src/python/core/login_manager.py
+++ TexDBook/src/python/core/login_manager.py
@@ -2,6 +2,7 @@ from flask_login import LoginManager
 from flask_paranoid import Paranoid
 
 from TexDBook.src.python.core.init_app import app, default_init_app
+from TexDBook.src.python.util.flask.rest_api import rest_api
 
 login_manager = LoginManager()  # type: LoginManager
 login_manager.init_app(app)
@@ -11,6 +12,7 @@ paranoid.init_app(app)
 
 
 @paranoid.on_invalid_session
+@rest_api
 def on_invalid_session():
     # type: () -> str
     print("Invalid Session")
