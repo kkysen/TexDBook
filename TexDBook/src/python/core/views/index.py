23a5fa823dc26582354dfd350a268198d8114a72
Khyber <kkysen@gmail.com>
2018-05-30 09:18:12 -0400

    Trying to get server to work.

--- TexDBook/src/python/core/views/index.py
+++ TexDBook/src/python/core/views/index.py
@@ -1,4 +1,4 @@
-from flask import Response, render_template
+from flask import Response, render_template, request
 
 from TexDBook.src.python.core.init_app import app, default_init_app
 from TexDBook.src.python.core.login_manager import paranoid
@@ -7,6 +7,8 @@ from TexDBook.src.python.core.login_manager import paranoid
 @app.route("/")
 def index():
     # type: () -> Response
+    print("\tINDEX\t")
+    print(request.__dict__)
     return render_template("index.html", debug=app.debug, csrf_token=paranoid.get_token())
 
 
