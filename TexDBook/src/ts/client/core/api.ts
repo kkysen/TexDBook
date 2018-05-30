6ef1ed01165f6cebf125a4dc3b1e51919837f87f
naotakak <naotakakinoshita@gmail.com>
2018-05-30 15:34:53 -0400

    wrote searchISBN

--- TexDBook/src/ts/client/core/api.ts
+++ TexDBook/src/ts/client/core/api.ts
@@ -150,3 +150,16 @@ export const api: TexDBookApi = {
     },
     
 };
+
+function searchISBN(isbn: string) : {title : string, author : string[], publisher : string,
+	 		  	    date: number, description : string, isbn: string} {
+	 let response = JSON.parse("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn);
+	 return {
+	 	title : response.volumeInfo.title;
+		author : response.volumeInfo.authors;
+		publisher : response.volumeInfo.publisher;
+		date : response.volumeInfo.publishedDate;
+		description : response.volumeInfo.description;
+		isbn : isbn;
+		};
+};
