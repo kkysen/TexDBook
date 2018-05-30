2f165805c7991f986ace6fc6cfdee814fa67d1d1
Khyber <kkysen@gmail.com>
2018-05-30 12:15:25 -0400

    Trying to get server to work.

--- TexDBook/src/python/core/login_manager.py
+++ TexDBook/src/python/core/login_manager.py
@@ -1,17 +1,13 @@
-from flask import Response
 from flask_login import LoginManager
 from flask_paranoid import Paranoid
 
 from TexDBook.src.python.core.init_app import app, default_init_app
-from TexDBook.src.python.core.views import index
-from TexDBook.src.python.util.flask.flask_utils import reroute_to
 
 login_manager = LoginManager()  # type: LoginManager
 login_manager.init_app(app)
 
 paranoid = Paranoid()  # type: Paranoid
 paranoid.init_app(app)
-paranoid.redirect_view = "/"
 
 
 @paranoid.on_invalid_session
