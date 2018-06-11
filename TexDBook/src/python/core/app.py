41c0e8d99b4f9eccbeca6d305e2616f9f1d4ee5a
Khyber <kkysen@gmail.com>
2018-06-11 10:06:27 -0400

    Trying to fix random log outs in Apache.

--- TexDBook/src/python/core/app.py
+++ TexDBook/src/python/core/app.py
@@ -5,13 +5,13 @@ from typing import Tuple
 
 from TexDBook.src.python.core import login_manager, models, static_gzip
 from TexDBook.src.python.core.init_app import NAME, app
-from TexDBook.src.python.core.views import books, data, index, login, users
+from TexDBook.src.python.core.views import books, data, login, users
 from TexDBook.src.python.util.flask.template_context import add_template_context
 
 
 def create_app():
     # type: () -> Tuple[Flask, str]
-    for module in [models, login_manager, index, static_gzip, data, login, books, users]:
+    for module in [models, login_manager, static_gzip, data, login, books, users]:
         module.init_app(app)
     add_template_context(app)
     return app, NAME
--- TexDBook/src/python/core/login_manager.py
+++ TexDBook/src/python/core/login_manager.py
@@ -1,8 +1,12 @@
+from flask import Response, render_template, request
 from flask_login import LoginManager
 from flask_paranoid import Paranoid
 
 from TexDBook.src.python.core.init_app import app, default_init_app
+from TexDBook.src.python.util.flask.flask_utils_types import JsonOrMessage
 from TexDBook.src.python.util.flask.rest_api import json, rest_api
+
+init_app = default_init_app
 
 login_manager = LoginManager()  # type: LoginManager
 login_manager.init_app(app)
@@ -12,12 +16,25 @@ paranoid.init_app(app)
 
 
 @paranoid.on_invalid_session
+def on_invalid_session():
+    # type: () -> Response
+    if request.path == "/":
+        return index()
+    else:
+        # noinspection PyTypeChecker
+        return rest_api_on_invalid_session()
+
+
 @json
 @rest_api
-def on_invalid_session():
-    # type: () -> str
-    print("Invalid Session")
+def rest_api_on_invalid_session():
+    # type: () -> JsonOrMessage
     return "Invalid Session"
 
 
-init_app = default_init_app
+@app.route("/")
+def index():
+    # type: () -> Response
+    print("INDEX")
+    # print(request.__dict__)
+    return render_template("index.html", debug=app.debug, csrf_token=paranoid.create_token())
