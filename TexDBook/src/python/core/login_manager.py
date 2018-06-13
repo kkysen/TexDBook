49c90867cdcd6539b01a7a7864ee5b7bc6d603b1
Khyber <kkysen@gmail.com>
2018-06-13 00:24:55 -0400

    Trying to fix repeated invalid sessions.

--- TexDBook/src/python/core/login_manager.py
+++ TexDBook/src/python/core/login_manager.py
@@ -7,6 +7,7 @@ from flask_paranoid import Paranoid
 from TexDBook.src.python.core.init_app import app, default_init_app
 from TexDBook.src.python.util.flask.flask_utils_types import JsonOrMessage
 from TexDBook.src.python.util.flask.rest_api import json, rest_api
+from TexDBook.src.python.util.oop import override
 
 init_app = default_init_app
 
@@ -15,6 +16,12 @@ login_manager.init_app(app)
 
 paranoid = Paranoid()  # type: Paranoid
 paranoid.init_app(app)
+
+
+@override(Paranoid)
+def write_token_to_session(_super, self, token):
+    print("writing token: " + token)
+    return _super(self, token)
 
 
 @paranoid.on_invalid_session
